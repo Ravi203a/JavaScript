b600e86f6b732bfdfdb5bd9c303fad39ac32b56f
Ravi203a <ravi182012@gmail.com>
2025-07-22 14:56:47 +0000

    added exercies

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JavaScript
 Javascript
+
+To Execute the script
+node <followed by path>
+Ex.
+node js_exercises/Level_101.js
---
+++ js_exercises/Level_101.js
@@ -0,0 +1,10 @@
+//Declare a variable named challenge and assign it to an initial value 'Hello World'.
+
+challenge = 'Hello World'
+console.log(challenge)
+
+//Print the string on the browser console using console.log()
+// Print the length of the string on the browser console using console.log()
+// Change all the string characters to capital letters using toUpperCase() method
+// Change all the string characters to lowercase letters using toLowerCase() method
+//Cut (slice) out the first word of the string using substr() or substring() method
